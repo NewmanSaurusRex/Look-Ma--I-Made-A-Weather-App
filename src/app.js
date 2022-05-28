7998604065d5d228d1587b6daa5c2701eefe5f39
NewmanSaurusRex <megan.m.boggs@gmail.com>
2022-05-27 18:51:42 -0700

    updated forecast function to search the new api call for daily data
    once daily data is received, display forecast for those days, up to the fifth day in the index

--- src/app.js
+++ src/app.js
@@ -21,28 +21,43 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDailyForecast(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` <div class="col-2">
-        <div class="next-day-info">${day}</div>
-        <img src="#" alt="#" width="38" 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+        <div class="next-day-info">${formatDailyForecast(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="#" width="38" 
         />
       
       <div class="next-day-temp">
-        <span class="weather-forecast-temp-max">18°</span>
-        <span class="weather-forecast-temp-min">12°</span>
+        <span class="weather-forecast-temp-max">${Math.round(
+          forecastDay.temp.max
+        )}°</span>
+        <span class="weather-forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
       </div>
     </div>
   `;
+    }
   });
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
