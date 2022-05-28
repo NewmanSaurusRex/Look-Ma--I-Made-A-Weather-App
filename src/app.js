68e44b3a28c6d5ac4503f7b5c021cb95a314c5a8
NewmanSaurusRex <megan.m.boggs@gmail.com>
2022-05-27 17:32:09 -0700

    linked axios to new api to receive forecast temp info
    getForecast now has an api call to gather coordinates from our searched city; these coords can be used to log / store info.

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
 
@@ -46,6 +47,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "a2c1979c00399a5761263bb164c05266";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp-data");
   let cityElement = document.querySelector("#city");
@@ -68,6 +75,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -111,4 +120,3 @@ let celsiusLink = document.querySelector("#metric-temp");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 search("Los Angeles");
-displayForecast();
